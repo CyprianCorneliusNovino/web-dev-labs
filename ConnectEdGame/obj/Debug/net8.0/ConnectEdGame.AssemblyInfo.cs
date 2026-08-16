e9f3196341c7b07cbf4c3b396a3f30b31bf3c36d
Cyprian Cornelius B. Novinoe <novinocyre@gmail.comm>
2026-08-17 04:06:17 +0800

    CONNECTED

--- ConnectEdGame/obj/Debug/net8.0/ConnectEdGame.AssemblyInfo.cs
+++ ConnectEdGame/obj/Debug/net8.0/ConnectEdGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c3331d4b2ac118f8b2ca900acbb40e04d50777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8dd67416e089e8bbfa3997f4cb496cc0f4d5a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
