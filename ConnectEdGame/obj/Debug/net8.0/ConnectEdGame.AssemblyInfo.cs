4a2c8b047ff6b04ed427938e6a1d343de84afc76
Cyprian Cornelius B. Novinoe <novinocyre@gmail.comm>
2026-08-20 23:54:23 +0800

    add direct thesis game link

--- ConnectEdGame/obj/Debug/net8.0/ConnectEdGame.AssemblyInfo.cs
+++ ConnectEdGame/obj/Debug/net8.0/ConnectEdGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f3196341c7b07cbf4c3b396a3f30b31bf3c36d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a386da0e3920e840b63b9ad3f35565d9ba80d323")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectEdGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
